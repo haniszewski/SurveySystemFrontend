2d2d089c238ed14d4117d5de5982620ee973b195
JanCzop <101407608+JanCzop@users.noreply.github.com>
2023-12-01 18:21:26 +0100

    Update logout-button.tsx

--- src/components/atoms/user-bar/logout-button.tsx
+++ src/components/atoms/user-bar/logout-button.tsx
@@ -1,1 +1,11 @@
+import { PowerIcon } from "@heroicons/react/24/outline";
 
+const LogoutButton = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <button type="button" onClick={onClick}>
+      <PowerIcon className="h-6 w-6 text-black-600" />
+    </button>
+  );
+};
+
+export default LogoutButton;
