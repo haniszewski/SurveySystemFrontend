c2a9106bb697077dbb3bbace908f835464f4d080
JanCzop <101407608+JanCzop@users.noreply.github.com>
2023-12-03 18:31:59 +0100

    Add files via upload

--- src/components/atoms/title-bar.tsx
+++ src/components/atoms/title-bar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import NavTab from "@/components/atoms/user-bar/nav-tab";
 
 const TitleBar = ({ title }: { title: string }) => {
   return (
-    <div className="flex h-full flex-wrap items-center justify-center bg-sky-700 p-6">
-      <div className="mr-6 flex flex-shrink-0 items-center text-white">
+    <div className="flex h-full items-center justify-between bg-sky-700 p-6">
+      <div className="flex flex-shrink-0 items-center text-white">
         <span className="text-3xl font-semibold tracking-tight">{title}</span>
+      </div>
+      <div className="flex flex-shrink-0 items-center text-white">
+        <NavTab />
       </div>
     </div>
   );
