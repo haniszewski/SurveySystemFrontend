ed4d78e2719570b9eb6fdade158528e9e2982db1
magda242 <242537@student.pwr.edu.pl>
2023-12-08 23:39:54 +0100

    Merge latest changes from development

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { Link } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+const Hero = ({ heading, message }) => {
+  const router = useRouter();
+  return (
+    <div className="custom-img mb-12 flex h-screen items-center justify-center bg-cover bg-fixed bg-center">
+      {/* Overlay */}
+      <div className="absolute bottom-0 left-0 right-0 top-0 z-[2] bg-black/70" />
+      <div className="z-[2] mt-[-10rem] p-5 text-white">
+        <h2 className="text-5xl font-bold">{heading}</h2>
+        <p className="py-5 text-xl">{message}</p>
+
+        <button
+          onClick={() => router.push("/auth/register")}
+          className="border px-8 py-2"
+        >
+          Zarejestruj się
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/atoms/user-bar/login-button.tsx
+++ src/components/atoms/user-bar/login-button.tsx
@@ -1,12 +1,19 @@
-"use client";
+// "use client";
 import { PowerIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const LoginButton = () => {
+  const router = useRouter();
   return (
     <Link href="/auth/login" className="flex items-center justify-center">
       <PowerIcon className="text-white-600 mr-2 h-6 w-6" />
-      <label className="text-white-600 cursor-pointer">Zaloguj</label>
+      <label
+        // onClick={() => router.push("auth/login")}
+        className="text-white-600 cursor-pointer"
+      >
+        Zaloguj
+      </label>
     </Link>
   );
 };
