471c75d3ae36dc23ee2d838eb3b04d7a7d1ec06b
JanCzop <101407608+JanCzop@users.noreply.github.com>
2023-12-14 12:25:01 +0100

    Update new-survey-header.tsx

--- src/components/molecules/create-survey/new-survey-header.tsx
+++ src/components/molecules/create-survey/new-survey-header.tsx
@@ -9,121 +9,119 @@ import TextFormField from "@/components/atoms/form/fields/text-form-field";
 
 const NewSurveyHeader = () => {
   const [isEditing, setIsEditing] = useState(false);
-  const { getValues, register } = useFormContext();
-
-  const [title, setTitle] = useState("Nowa ankieta");
-  const [description, setDescription] = useState("Proszę wypełnić ankietę");
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const { getValues } = useFormContext();
 
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
   const handleSaveClick = () => {
-    setTitle(getValues("surveyTitle") ?? "Moja ankieta");
-    setDescription(getValues("surveyDescription") ?? "");
-    setStartDate(dayjs(getValues("surveyStartDate")).format("YYYY-MM-DD"));
-    setEndDate(dayjs(getValues("surveyEndDate")).format("YYYY-MM-DD"));
-    /*
-    console.log("Title:", title);
-    console.log("Description:", description);
-    console.log("Start Date:", startDate);
-    console.log("Start Date:", endDate);
-*/
     setIsEditing(false);
+  };
+
+  const isValidDate = (dateString: string | undefined) => {
+    if (!dateString) {
+      return false;
+    }
+    const date = new Date(dateString);
+    return !isNaN(date.getTime());
   };
 
   return (
     <div>
       {isEditing ? (
         <>
-          <div className="mb-4 w-full rounded-lg bg-white p-6 text-left shadow">
-            <label
-              htmlFor="surveyTitle"
-              className="block text-sm font-bold text-gray-600"
-            >
-              Podaj tytuł ankiety
-            </label>
-            <TextFormField
-              name="surveyTitle"
-              type="text"
-              className="w-full rounded-md border px-3 py-2 focus:border-blue-500 focus:outline-none"
-              required
-            />
-          </div>
-          <div className="mb-4 w-full rounded-lg bg-white p-6 text-left shadow">
-            <label
-              htmlFor="surveyDescription"
-              className="block text-sm font-bold text-gray-600"
-            >
-              Podaj opis ankiety
-            </label>
-            <TextFormField
-              name="surveyDescription"
-              type="text"
-              className="w-full rounded-md border px-3 py-2 focus:border-blue-500 focus:outline-none"
-            />
-          </div>
-          <div className="mb-4 w-full rounded-lg bg-white p-6 text-left shadow">
-            <label
-              htmlFor="surveyStartDate"
-              className="block text-sm font-bold text-gray-600"
-            >
-              Podaj datę rozpoczęcia ankiety
-            </label>
-            <TextFormField
-              name="surveyStartDate"
-              type="date"
-              className="w-full rounded-md border px-3 py-2 focus:border-blue-500 focus:outline-none"
-            />
-          </div>
-          <div className="mb-4 w-full rounded-lg bg-white p-6 text-left shadow">
-            <label
-              htmlFor="surveyEndDate"
-              className="block text-sm font-bold text-gray-600"
-            >
-              Podaj datę zakończenia ankiety
-            </label>
-            <TextFormField
-              name="surveyEndDate"
-              type="date"
-              className="w-full rounded-md border px-3 py-2 focus:border-blue-500 focus:outline-none"
-            />
-          </div>
+          <div className="flex w-full items-center justify-between rounded-md bg-white p-5 shadow">
+            <div className="w-5/6">
+              <div className="mb-4 w-full rounded-lg bg-white p-6 text-left shadow">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-bold text-gray-600"
+                >
+                  Podaj tytuł ankiety
+                </label>
+                <TextFormField
+                  name="name"
+                  type="text"
+                  className="w-full rounded-md border px-3 py-2 focus:border-blue-500 focus:outline-none"
+                  required
+                />
+              </div>
+              <div className="mb-4 w-full rounded-lg bg-white p-6 text-left shadow">
+                <label
+                  htmlFor="start_date"
+                  className="block text-sm font-bold text-gray-600"
+                >
+                  Podaj datę rozpoczęcia ankiety
+                </label>
+                <TextFormField
+                  name="start_date"
+                  type="date"
+                  className="w-full rounded-md border px-3 py-2 focus:border-blue-500 focus:outline-none"
+                />
+              </div>
+              <div className="mb-4 w-full rounded-lg bg-white p-6 text-left shadow">
+                <label
+                  htmlFor="end_date"
+                  className="block text-sm font-bold text-gray-600"
+                >
+                  Podaj datę zakończenia ankiety
+                </label>
+                <TextFormField
+                  name="end_date"
+                  type="date"
+                  className="w-full rounded-md border px-3 py-2 focus:border-blue-500 focus:outline-none"
+                />
+              </div>
+            </div>
 
-          <div className="d-flex ml-4 justify-end">
-            <SaveButton onClick={handleSaveClick} />
+            <div className="buttons flex w-1/6 justify-end gap-2">
+              <SaveButton onClick={handleSaveClick} />
+            </div>
           </div>
         </>
       ) : (
         <>
-          <div className="mb-4 w-full rounded-lg bg-white p-6 text-left shadow">
-            <h1 className="mb-4 text-4xl font-bold">Tytuł: {title}</h1>
-            <p className="text-gray text-lg">
-              <span className="font-bold">Opis:</span> {description}
-            </p>
-            <p className="mt-2 text-sm text-black">
-              <span className="font-bold">Rozpoczęcie:</span>{" "}
-              <span className="hidden md:inline">
-                {dayjs(startDate).format("YYYY-MM-DD")}
-              </span>
-              <span className="block md:hidden">
-                {dayjs(startDate).format("YYYY-MM-DD")}
-              </span>
-            </p>
-            <p className="mt-2 text-sm text-black">
-              <span className="font-bold">Zakończenie:</span>{" "}
-              <span className="hidden md:inline">
-                {dayjs(endDate).format("YYYY-MM-DD")}
-              </span>
-              <span className="block md:hidden">
-                {dayjs(endDate).format("YYYY-MM-DD")}
-              </span>
-            </p>
-          </div>
-          <div className="d-flex ml-4 justify-end">
-            <EditButton onClick={handleEditClick} />
+          <div className="new-question-block group flex w-full items-center justify-between rounded-md bg-white p-5 shadow">
+            <div className="w-5/6">
+              <div className="mb-4 w-full rounded-lg bg-white p-6 text-left shadow">
+                <h1 className="mb-4 text-4xl font-bold">
+                  Tytuł: {getValues("name") || "Moja ankieta"}
+                </h1>
+                <p className="mt-2 text-sm text-black">
+                  <span className="font-bold">Rozpoczęcie:</span>{" "}
+                  <span className="hidden md:inline">
+                    {getValues("start_date") &&
+                    isValidDate(getValues("start_date"))
+                      ? dayjs(getValues("start_date")).format("YYYY-MM-DD")
+                      : "Data niepodana"}
+                  </span>
+                  <span className="block md:hidden">
+                    {getValues("start_date") &&
+                    isValidDate(getValues("start_date"))
+                      ? dayjs(getValues("start_date")).format("YYYY-MM-DD")
+                      : "Data niepodana"}
+                  </span>
+                </p>
+                <p className="mt-2 text-sm text-black">
+                  <span className="font-bold">Zakończenie:</span>{" "}
+                  <span className="hidden md:inline">
+                    {getValues("end_date") && isValidDate(getValues("end_date"))
+                      ? dayjs(getValues("end_date")).format("YYYY-MM-DD")
+                      : "Data niepodana"}
+                  </span>
+                  <span className="block md:hidden">
+                    {getValues("start_date") &&
+                    isValidDate(getValues("end_date"))
+                      ? dayjs(getValues("end_date")).format("YYYY-MM-DD")
+                      : "Data niepodana"}
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className="buttons flex w-1/6 justify-end gap-2">
+              <EditButton onClick={handleEditClick} />
+            </div>
           </div>
         </>
       )}
