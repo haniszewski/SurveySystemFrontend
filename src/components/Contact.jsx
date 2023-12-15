45c3b73a799aa26950c47f231643a5a6993ab82a
DawidZ <dawid.ziobrowski@brandmed.com>
2023-12-15 13:45:30 +0100

    Fix critical error

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div className='max-w-[1240px] m-auto p-4 h-screen'>
-        <h1 className='text-2xl font-bold text-center p-4'>Prosimy o zgłaszanie sugestii</h1>
-        <form className='max-w-[600px] m-auto'>
-            <div className='grid grid-cols-2 gap-2'>
-                <input className='border shadow-lg p-3' type="text" placeholder='Imię' />
-                <input className='border shadow-lg p-3' type="email" placeholder='Email' />
-            </div>
-            <input className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Temat' />
-            <textarea className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Wiadomość'></textarea>
-            <button className='border shadow-lg p-3 w-full mt-2'>Wyślij</button>
-        </form>
+    <div className="m-auto h-screen max-w-[1240px] p-4">
+      <h1 className="p-4 text-center text-2xl font-bold">
+        Prosimy o zgłaszanie sugestii
+      </h1>
+      <form className="m-auto max-w-[600px]">
+        <div className="grid grid-cols-2 gap-2">
+          <input
+            className="border p-3 shadow-lg"
+            type="text"
+            placeholder="Imię"
+          />
+          <input
+            className="border p-3 shadow-lg"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <input
+          className="my-2 w-full border p-3 shadow-lg"
+          type="text"
+          placeholder="Temat"
+        />
+        <textarea
+          className="w-full border p-3 shadow-lg"
+          cols="30"
+          rows="10"
+          placeholder="Wiadomość"
+        ></textarea>
+        <button className="mt-2 w-full border p-3 shadow-lg">Wyślij</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
